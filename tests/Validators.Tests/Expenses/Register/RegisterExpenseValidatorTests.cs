8e1258d2397678d6ebc8e74b140228b224515547
Otavin96 <otaviolazzarotto@icloud.com>
2025-08-08 11:47:44 -0300

    criando o primeiro teste

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,27 @@
+﻿using CashFlow.Application.UseCase.Expense.Register;
+using CashFlow.Communication.Requests;
+
+public class RegisterExpenseValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+
+        //Arrange
+        var validator = new RegisterExpenseValidator();
+        var request = new RequestRegisterExpenseJson
+        {
+            Amount = 100,
+            Date = DateTime.Now.AddDays(-1),
+            Description = "Description",
+            PaymentType = CashFlow.Communication.Enums.PaymentType.CreditCard,
+            Title = "Apple",
+        };
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        Assert.True(result.IsValid);
+    }
+}
