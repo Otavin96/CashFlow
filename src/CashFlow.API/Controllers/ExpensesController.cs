61a41717338d978c1e973cdf94ec6aa082c7fb8f
Otavin96 <otaviolazzarotto@icloud.com>
2025-08-07 22:56:27 -0300

    Adicionar arquivos de projeto.

---
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Application.UseCase.Expense.Register;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        var UseCase = new RegisterExpenseUseCase();
+
+        var response = UseCase.Execute(request);
+        return Created(string.Empty, response);
+    }
+}
---
+++ src/CashFlow.API/Program.cs
@@ -0,0 +1,45 @@
+﻿using CashFlow.API.Filters;
+using CashFlow.API.Middleware;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Minha API",
+        Version = "v1", // 👈 ESTA É A VERSÃO DA SUA DOCUMENTAÇÃO SWAGGER
+        Description = "Documentação da API gerada com Swagger",
+    });
+});
+
+builder.Services.AddRouting(option => option.LowercaseUrls = true);
+
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<CultureMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
